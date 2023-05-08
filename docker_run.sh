2937d8bc2f837bd3a03d04130df049fd3d62f5bc
nizerlak <prdabr@gmail.com>
2023-05-08 21:13:12 +0200

    Correct shebang

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/sh
+#!/bin/sh
 docker build -t argo:dzik .
 docker run --rm --network=host -it --expose 11345 -v ${PWD}/src:${PWD}/src -v ${PWD}/run_sim.sh:${PWD}/run_sim.sh --workdir ${PWD} argo:dzik "./run_sim.sh"
