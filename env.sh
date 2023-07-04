23bfc8d4e22836338d2c78cde80b5f9507403a86
waleczna_lama <rafal.krynski13@gmail.com>
2023-07-04 18:13:44 +0200

    raspi benchtest launch

---
+++ env.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+source /home/rafpi/ws_dzik/devel/setup.bash
+
+#export ROS_HOSTNAME=192.168.0.100
+#export ROS_IP=192.168.0.100
+#export ROS_MASTER_URI=http://192.168.0.102:11311
+
+export ROS_HOSTNAME=192.168.1.18
+export ROS_IP=192.168.1.18
+export ROS_MASTER_URI=http://192.168.1.11:11311
+
+exec "$@"
